d4692f5857ba5614b05533a872ae46db06b13d70
danielshih <dog830228@gmail.com>
2025-06-05 23:23:40 +0800

    add more example.

--- marco_example/src/main.rs
+++ marco_example/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 macro_rules! ops {
     ($v1:literal plus $v2:literal) => {
         $v1 + $v2
@@ -13,6 +15,35 @@ macro_rules! ops {
     };
 }
 
+macro_rules! if_any {
+    ($($condition:expr),+ ;$block:block) => {
+        if $($condition) || +
+            $block
+    };
+}
+
+macro_rules! hashmap {
+    ($($key:literal => $value:expr,)*) => {
+        {
+            let mut map = HashMap::new();
+            $(map.insert($key,$value);)*
+            map
+        }
+    };
+}
+
 fn main() {
     println!("{}, {}", ops!(100 mutiply 10), ops!(100 plus 10));
+
+    if_any!(false, 0 == 1, true; {
+        println!("Yay, the if statement worked.");
+    });
+
+    let value = "hello";
+    let my_hashmap = hashmap!(
+        "hash" => "map",
+        "Key" => value,
+    );
+
+    println!("{my_hashmap:#?}");
 }
