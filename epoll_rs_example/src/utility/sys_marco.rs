34eacdaf1d0053f470edd8f259e2e1e99525a806
Danielshih <dog830228@gmail.com>
2025-06-19 08:04:57 +0000

    refactor

---
+++ epoll_rs_example/src/utility/sys_marco.rs
@@ -0,0 +1,18 @@
+#[macro_export]
+macro_rules! syscall {
+    ($fn: ident ( $($arg: expr),* $(,)* ) ) => {{
+        let res = unsafe { libc::$fn($($arg, )*) };
+        if res == -1 {
+            Err(std::io::Error::last_os_error())
+        } else {
+            Ok(res)
+        }
+    }};
+    ( $($fn: ident ( $($arg: expr),* $(,)* )),* $(,)* ) => {{
+        unsafe{
+            $(
+                libc::$fn($($arg, )*);
+            )*
+        }
+    }};
+}
